e49e3cdcec82d57bc81587bdc3331406ac786e15
Safwan-Hayaat <safwanhayaat@gmail.com>
2026-03-02 16:30:26 +0600

    added a code for form submission
    used formspree

--- Scripts/main.js
+++ Scripts/main.js
@@ -33,10 +33,9 @@ updateHeaderPosition();
 */
 
 function showSidebar() {
-    const sidebar = document.querySelector('.sidebar')
-    sidebar.style.display ='flex'
+  const sidebar = document.querySelector(".sidebar");
+  sidebar.style.display = "flex";
 }
-
 
 function hideSidebar() {
   const sidebar = document.querySelector(".sidebar");
@@ -44,3 +43,54 @@ function hideSidebar() {
 }
 
 
+
+/*This is a js code to send me a email everytime someone fills out the form on join.html*/ 
+    const clubForm = document.getElementById('ict-club-form');
+    const statusMsg = document.getElementById('status-msg');
+    const btn = document.getElementById('submitBtn');
+
+    clubForm.addEventListener('submit', async function(e) {
+        e.preventDefault();
+        
+        const formData = new FormData(this);
+        btn.disabled = true;
+        btn.innerText = "Sending...";
+
+        // Connection to formspree
+        const endpoint1 = "https://formspree.io/f/xdadobra";
+        const endpoint2 = "https://formspree.io/f/mqedodey";
+
+        try {
+            const request1 = fetch(endpoint1, {
+                method: 'POST',
+                body: formData,
+                headers: { 'Accept': 'application/json' }
+            });
+
+            const request2 = fetch(endpoint2, {
+                method: 'POST',
+                body: formData,
+                headers: { 'Accept': 'application/json' }
+            });
+
+            const [response1, response2] = await Promise.all([request1, request2]);
+
+            if (response1.ok || response2.ok) {
+                statusMsg.style.display = "block";
+                statusMsg.style.backgroundColor = "#d4edda";
+                statusMsg.style.color = "#155724";
+                statusMsg.innerText = "Your form was successfully received";
+                clubForm.reset();
+            } else {
+                throw new Error("Submission Failed");
+            }
+        } catch (error) {
+            statusMsg.style.display = "block";
+            statusMsg.style.backgroundColor = "#f8d7da";
+            statusMsg.style.color = "#721c24";
+            statusMsg.innerText = "Your form failed to send";
+        } finally {
+            btn.disabled = false;
+            btn.innerText = "Fill out the form";
+        }
+    });
